da6ec46b8b722f95dc1a1adb5136d10213e812ae
wd-libraries <42412694+wd-libraries@users.noreply.github.com>
2018-08-16 15:36:26 +0200

    Initial commit

---
+++ setup12e.ino
@@ -0,0 +1,15 @@
+const uint8_t BUILTIN_LED2 = 2; //GPIO2
+
+void setup() {
+  pinMode(BUILTIN_LED2, OUTPUT);
+  pinMode(D7, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(D7, HIGH);
+  digitalWrite(BUILTIN_LED2, HIGH);
+  delay(1000);
+  digitalWrite(D7, LOW);
+  digitalWrite(BUILTIN_LED2, LOW);
+  delay(1000);
+}
